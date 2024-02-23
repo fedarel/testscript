28ba4bfefb18907c1277181b0cca85090827cd8b
federal <140007438+fedarel@users.noreply.github.com>
2024-02-23 17:37:34 -0500

    Create .lua

---
+++ .lua
@@ -0,0 +1,13 @@
+--// Test Script
+
+	game.StarterGui:SetCore("SendNotification", {
+		Title = "New Message!",
+		Text = "Macsploit is currently working.",
+		Button1 = "Continue",
+		Duration = 1 / 0,
+		Icon = "rbxassetid://6238537240",
+		Callback = Aux
+	})
+end
+
+--// made by Fedarel
