636e728afd163fda6871501a711c82ee0ec01499
federal <140007438+fedarel@users.noreply.github.com>
2024-03-03 22:39:05 -0500

    Update macsploit.lua

--- macsploit.lua
+++ macsploit.lua
@@ -1,26 +1,33 @@
-local scriptText = [==[
-    local PromptFunction = Instance.new("BindableFunction")
-    
-    PromptFunction.OnInvoke = function(response)
-        if response == "No" then
-            return
-        end
-        
-        print("Notification: MacSploit Is Working!")
-    end
-
-    game.StarterGui:SetCore("SendNotification", {
-        Title = "Test Script Prompt",
-        Text = "MacSploit is Working",
-        Button1 = "Continue",
-        Duration = math.huge,
-        Icon = "rbxassetid://6238537240",
-        Callback = PromptFunction
-    })
-]==]
-
-local function executeScript(script)
-    loadstring(script)() -- Execute the script
+local function getCurrentDate()
+    local utc_time = os.time()
+    local est_offset = -5 * 3600
+    local est_time = utc_time + est_offset
+    local est_formatted_date = os.date("%Y-%m-%d", est_time)
+    local utc_formatted_date = os.date("%Y-%m-%d", utc_time)
+    return utc_formatted_date, est_formatted_date
 end
 
-executeScript(scriptText)
+local utc_date, est_date = getCurrentDate()
+
+local text = "Test Script is currently working on " .. est_date
+
+local PromptFunction = Instance.new("BindableFunction")
+
+PromptFunction.OnInvoke = function(response)
+    if response == "Continue" then
+        print("Continuing...")
+    else
+        print("Notification dismissed.")
+    end
+end
+
+print("Developed by fedarel, for Macsploit")
+
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Test Script Prompt",
+    Text = text,
+    Button1 = "Continue",
+    Duration = math.huge,
+    Icon = "rbxassetid://6238537240",
+    Callback = PromptFunction
+})
